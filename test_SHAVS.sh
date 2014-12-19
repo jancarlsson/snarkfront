b12aae958321f3bad59b3fb7b436ffc018e9150e
jancarlsson <jancarlsson@Safe-mail.net>
2014-12-19 19:32:23 +0000

    Create test_SHAVS.sh

---
+++ test_SHAVS.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+case $# in
+  1) DIR=$1 ;;
+  0) echo "usage: "$0" path_to_SHAVS_byte_test_vector_files"
+     exit
+esac
+
+for BITS in 1 224 256 384 512
+do
+  echo
+  echo "SHA"$BITS" Monte Carlo"
+  cat $DIR"/SHA"$BITS"Monte.txt" | ./test_SHAVS -c BN128 -b $BITS
+done
+
+for BITS in 1 224 256 384 512
+do
+  echo
+  echo "SHA"$BITS" short messages"
+  cat $DIR"/SHA"$BITS"ShortMsg.rsp" | ./test_SHAVS -c BN128 -b $BITS
+done
+
+for BITS in 1 224 256 384 512
+do
+  echo
+  echo "SHA"$BITS" long messages"
+  cat $DIR"/SHA"$BITS"LongMsg.rsp" | ./test_SHAVS -c BN128 -b $BITS
+done
