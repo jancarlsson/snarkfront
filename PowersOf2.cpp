bdee0f40c64399bfac1aae8e5e548cc7a8cccff0
jancarlsson <jancarlsson@Safe-mail.net>
2014-12-19 19:21:11 +0000

    Create PowersOf2.cpp

---
+++ PowersOf2.cpp
@@ -0,0 +1,39 @@
+#include "PowersOf2.hpp"
+
+using namespace std;
+
+namespace snarkfront {
+
+size_t sizeBits(const bool& dummy) { return 1; }
+size_t sizeBits(const uint32_t& dummy) { return 32; }
+size_t sizeBits(const uint64_t& dummy) { return 64; }
+
+vector<int> valueBits(const bool& a) { return vector<int>(1, a); }
+
+template <typename UINT_N>
+vector<int> valueBits_internal(const UINT_N& a) {
+    vector<int> v;
+    v.reserve(sizeBits(a));
+
+    UINT_N b = a;
+    for (size_t i = 0; i < sizeBits(b); ++i) {
+        v.push_back(b & 0x1);
+        b >>= 1;
+    }
+
+    return v;
+}
+    
+vector<int> valueBits(const uint32_t& a) { return valueBits_internal(a); }
+vector<int> valueBits(const uint64_t& a) { return valueBits_internal(a); }
+    
+size_t countBits(const vector<int>& v) {
+    size_t count = 0;
+
+    for (const auto& b : v)
+        count += b;
+
+    return count;
+}
+
+} // namespace snarkfront
