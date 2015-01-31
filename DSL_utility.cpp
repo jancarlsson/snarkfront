1c2a04237211b89429b4f85587efc4cb36986210
jancarlsson <jancarlsson@Safe-mail.net>
2015-01-31 18:30:27 +0000

    validate SHA-2 names

--- DSL_utility.cpp
+++ DSL_utility.cpp
@@ -20,4 +20,36 @@ bool pairingEdwards(const string& name) {
     return "Edwards" == name;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// SHA-2
+//
+
+bool validSHA2Name(const string& shaBits) {
+    return
+        ("1" == shaBits) ||
+        ("224" == shaBits) ||
+        ("256" == shaBits) ||
+        ("384" == shaBits) ||
+        ("512" == shaBits) ||
+        ("512_224" == shaBits) ||
+        ("512_256" == shaBits);
+}
+
+bool validSHA2Name(const size_t shaBits) {
+    return
+        (1 == shaBits) ||
+        (224 == shaBits) ||
+        (256 == shaBits) ||
+        (384 == shaBits) ||
+        (512 == shaBits);
+}
+
+bool nameSHA256(const string& shaBits) {
+    return "256" == shaBits;
+}
+
+bool nameSHA512(const string& shaBits) {
+    return "512" == shaBits;
+}
+
 } // namespace snarkfront
