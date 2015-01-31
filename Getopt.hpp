cca0ac674f4082890a2a360b1241a0e08fb1b863
jancarlsson <jancarlsson@Safe-mail.net>
2015-01-31 18:27:48 +0000

    CLI getopt helper

---
+++ Getopt.hpp
@@ -0,0 +1,44 @@
+#ifndef _SNARKFRONT_GETOPT_HPP_
+#define _SNARKFRONT_GETOPT_HPP_
+
+#include <cstdint>
+#include <map>
+#include <set>
+#include <string>
+
+namespace snarkfront {
+
+////////////////////////////////////////////////////////////////////////////////
+// command line handling
+//
+
+class Getopt
+{
+public:
+    Getopt(int argc, char *argv[],
+           const std::string& string_opts,
+           const std::string& number_opts,
+           const std::string& flag_opts);
+
+    bool operator! () const;
+    bool empty() const;
+
+    bool valid(const std::string& string_opts,
+               const std::string& number_opts,
+               const std::string& flag_opts);
+
+    std::string getString(const char c);
+    std::size_t getNumber(const char c);
+    bool getFlag(const char c);
+
+private:
+    std::map<int, std::string> m_string;
+    std::map<int, std::size_t> m_number;
+    std::set<int> m_flag, m_string_opts, m_number_opts, m_flag_opts;
+
+    bool m_error;
+};
+
+} // namespace snarkfront
+
+#endif
