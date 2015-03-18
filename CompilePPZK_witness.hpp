d5199ea61ea696055c39c9a42d1cbcbe03823862
jancarlsson <jancarlsson@Safe-mail.net>
2015-03-18 21:37:38 +0000

    refactor CompilePPZK.hpp

---
+++ CompilePPZK_witness.hpp
@@ -0,0 +1,211 @@
+#ifndef _SNARKFRONT_COMPILE_PPZK_WITNESS_HPP_
+#define _SNARKFRONT_COMPILE_PPZK_WITNESS_HPP_
+
+#include <cstdint>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "snarkfront.hpp"
+
+namespace snarkfront {
+
+////////////////////////////////////////////////////////////////////////////////
+// witnesses A, B, C
+//
+
+template <typename WITNESS, typename PAIRING>
+class PPZK_witness_ABC
+{
+    typedef typename PAIRING::Fr FR;
+
+public:
+    PPZK_witness_ABC(const std::string& sysfile,
+                     const std::string& randfile,
+                     const std::string& witfile)
+        : m_error(false),
+          m_numVariables(0),
+          m_reserveTune(0),
+          m_hugeSystem(sysfile)
+    {
+        std::ifstream ifsR(randfile), ifsW(witfile);
+        if (!ifsR || !m_randomness.marshal_in(ifsR) ||
+            !ifsW || !m_witness.marshal_in(ifsW) ||
+            !m_hugeSystem.loadIndex())
+        {
+            m_error = true;
+        }
+        else
+        {
+            const snarklib::QAP_SystemPoint<snarklib::HugeSystem, FR>
+                qap(m_hugeSystem,
+                    m_hugeSystem.numCircuitInputs());
+
+            m_numVariables = qap.numVariables();
+        }
+    }
+
+    bool operator! () const { return m_error; }
+
+    void initA() {
+        m_sum = WITNESS(m_numVariables, m_witness, m_randomness.d1());
+    }
+
+    void initB() {
+        m_sum = WITNESS(m_numVariables, m_witness, m_randomness.d2());
+    }
+
+    void initC() {
+        m_sum = WITNESS(m_numVariables, m_witness, m_randomness.d3());
+    }
+
+    void reserveTune(const std::size_t n = 0) {
+        m_reserveTune = n;
+    }
+
+    void accumQuery(const std::string& queryfile,
+                    const std::size_t blocknum,
+                    ProgressCallback* callback = nullptr)
+    {
+
+        typename WITNESS::SparseVec query;
+
+        std::stringstream ss;
+        ss << queryfile << blocknum;
+
+        std::ifstream ifs(ss.str());
+        if (!ifs || !query.marshal_in(ifs)) {
+            m_error = true;
+
+        } else {
+            if (callback) callback->major(true);
+            m_sum.accumQuery(query,
+                             m_reserveTune,
+                             callback);
+        }
+    }
+
+    typename WITNESS::Val val() const {
+        return m_sum.val();
+    }
+
+private:
+    bool m_error;
+    std::size_t m_numVariables, m_reserveTune;
+    snarklib::HugeSystem<FR> m_hugeSystem;
+    snarklib::PPZK_ProofRandomness<FR> m_randomness;
+    snarklib::R1Witness<FR> m_witness;
+    WITNESS m_sum;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// witness H
+//
+
+template <typename PAIRING>
+class PPZK_witness_H
+{
+    typedef typename PAIRING::Fr FR;
+    typedef typename PAIRING::G1 G1;
+
+public:
+    PPZK_witness_H(const std::string& qapABCH)
+        : m_error(false),
+          m_qapABCH(qapABCH)
+    {}
+
+    bool operator! () const { return m_error; }
+
+    void accumQuery(const std::string& queryfile,
+                    const std::size_t blocknum,
+                    ProgressCallback* callback = nullptr)
+    {
+        snarklib::BlockVector<G1> query;
+        snarklib::BlockVector<FR> scalar;
+        if (!snarklib::read_blockvector(queryfile, blocknum, query) ||
+            !snarklib::read_blockvector(m_qapABCH, blocknum, scalar)) {
+            m_error = true;
+
+        } else {
+            if (callback) callback->major(true);
+            m_sum.accumQuery(query,
+                             scalar,
+                             callback);
+        }
+    }
+
+    const G1& val() const {
+        return m_sum.val();
+    }
+
+private:
+    bool m_error;
+    const std::string m_qapABCH;
+    snarklib::PPZK_WitnessH<PAIRING> m_sum;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// witness K
+//
+
+template <typename PAIRING>
+class PPZK_witness_K
+{
+    typedef typename PAIRING::Fr FR;
+    typedef typename PAIRING::G1 G1;
+
+public:
+    PPZK_witness_K(const std::string& randfile,
+                   const std::string& witfile)
+        : m_error(false),
+          m_reserveTune(0)
+    {
+        std::ifstream ifsR(randfile), ifsW(witfile);
+        if (!ifsR || !m_randomness.marshal_in(ifsR) ||
+            !ifsW || !m_witness.marshal_in(ifsW)) {
+            m_error = true;
+
+        } else {
+            m_sum = snarklib::PPZK_WitnessK<PAIRING>(m_witness,
+                                                     m_randomness.d1(),
+                                                     m_randomness.d2(),
+                                                     m_randomness.d3());
+        }
+    }
+
+    bool operator! () const { return m_error; }
+
+    void reserveTune(const std::size_t n = 0) {
+        m_reserveTune = n;
+    }
+
+    void accumQuery(const std::string& queryfile,
+                    const std::size_t blocknum,
+                    ProgressCallback* callback = nullptr)
+    {
+        snarklib::BlockVector<G1> query;
+        if (!snarklib::read_blockvector(queryfile, blocknum, query)) {
+            m_error = true;
+
+        } else {
+            if (callback) callback->major(true);
+            m_sum.accumQuery(query,
+                             m_reserveTune,
+                             callback);
+        }
+    }
+
+    const G1& val() const {
+        return m_sum.val();
+    }
+
+private:
+    bool m_error;
+    std::size_t m_reserveTune;
+    snarklib::PPZK_ProofRandomness<FR> m_randomness;
+    snarklib::R1Witness<FR> m_witness;
+    snarklib::PPZK_WitnessK<PAIRING> m_sum;
+};
+
+} // namespace snarkfront
+
+#endif
