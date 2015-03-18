23eb7e038435e0a0119261ebd2421015e619f3d2
jancarlsson <jancarlsson@Safe-mail.net>
2015-03-18 21:59:05 +0000

    8-bit unsigned int array pre-image

--- SHA_256.hpp
+++ SHA_256.hpp
@@ -16,15 +16,18 @@ namespace snarkfront {
 // SHA-256
 //
 
-template <typename T, typename MSG, typename F>
-class SHA_256 : public SHA_Base<SHA_256<T, MSG, F>,
+template <typename T, typename MSG, typename U, typename F>
+class SHA_256 : public SHA_Base<SHA_256<T, MSG, U, F>,
                                 SHA_BlockSize::BLOCK_512,
                                 MSG>
 {
 public:
     typedef T WordType;
+    typedef U ByteType;
+
     typedef std::array<T, 16> MsgType;
     typedef std::array<T, 8> DigType;
+    typedef std::array<U, 16 * 4> PreType;
 
     SHA_256() {
         initConstants();
@@ -181,6 +184,7 @@ namespace zk {
     template <typename FR> using
     SHA256 = SHA_256<AST_Var<Alg_uint32<FR>>,
                      Lazy<AST_Var<Alg_uint32<FR>>, std::uint32_t>,
+                     AST_Var<Alg_uint8<FR>>,
                      SHA_Functions<AST_Node<Alg_uint32<FR>>,
                                    AST_Op<Alg_uint32<FR>>,
                                    BitwiseAST<Alg_uint32<FR>, Alg_uint32<FR>>>>;
@@ -189,6 +193,7 @@ namespace zk {
 namespace eval {
 typedef SHA_256<std::uint32_t,
                 std::uint32_t,
+                std::uint8_t,
                 SHA_Functions<std::uint32_t,
                               std::uint32_t,
                               BitwiseINT<std::uint32_t, std::uint32_t>>>
