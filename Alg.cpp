16961bab626ee91f045b99e630ccae2f1655d24b
jancarlsson <jancarlsson@Safe-mail.net>
2015-03-18 21:33:18 +0000

    overloaded valueToString (8-bit special case)

---
+++ Alg.cpp
@@ -0,0 +1,41 @@
+#include "Alg.hpp"
+
+using namespace std;
+
+namespace snarkfront {
+
+////////////////////////////////////////////////////////////////////////////////
+// valueToString
+//
+
+string valueToString(const bool& a) {
+    stringstream ss;
+    ss << a;
+    return ss.str();
+}
+
+string valueToString(const snarklib::BigInt<2>& a) {
+    stringstream ss;
+    ss << a;
+    return ss.str();
+}
+
+string valueToString(const uint32_t& a) {
+    stringstream ss;
+    ss << a;
+    return ss.str();
+}
+
+string valueToString(const uint64_t& a) {
+    stringstream ss;
+    ss << a;
+    return ss.str();
+}
+
+string valueToString(const uint8_t& a) {
+    // must handle uint8_t as a special case
+    // stream insertion regards it as text instead of numeric
+    return valueToString(static_cast<uint32_t>(a));
+}
+
+} // namespace snarkfront
