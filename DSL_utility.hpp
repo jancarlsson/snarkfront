8d37f64687c189c7b046010e82567faf85488bd5
jancarlsson <jancarlsson@Safe-mail.net>
2015-04-02 01:29:37 +0000

    lookup table for unsigned integer types

--- DSL_utility.hpp
+++ DSL_utility.hpp
@@ -7,7 +7,9 @@
 #include <istream>
 #include <ostream>
 #include <vector>
+#include "Alg.hpp"
 #include "AST.hpp"
+#include "BitwiseAST.hpp"
 
 namespace snarkfront {
 
@@ -39,6 +41,13 @@ bool nameSHA256(const std::string& shaBits);
 
 // returns true if "SHA512"
 bool nameSHA512(const std::string& shaBits);
+
+////////////////////////////////////////////////////////////////////////////////
+// AES
+//
+
+// check if: 128, 192, 256
+bool validAESName(const std::size_t aesBits);
 
 ////////////////////////////////////////////////////////////////////////////////
 // powers of 2
@@ -127,6 +136,64 @@ DEFN_VECTOR_ARRAY_IN(std::uint32_t)
 DEFN_VECTOR_ARRAY_IN(std::uint64_t)
 
 #undef DEFN_VECTOR_ARRAY_IN
+
+////////////////////////////////////////////////////////////////////////////////
+// lookup table for unsigned integer types
+//
+
+template <typename T, typename U, typename VAL, typename BITWISE>
+class BitwiseLUT
+{
+public:
+    template <std::size_t N>
+    BitwiseLUT(const std::array<VAL, N>& table_elements)
+        : m_value(table_elements.begin(),
+                  table_elements.end())
+    {}
+
+    std::size_t size() const { return m_value.size(); }
+
+    U operator[] (const T& x) const
+    {
+        const auto N = m_value.size();
+
+        auto sum = BITWISE::_AND(BITWISE::_constant(m_value[0]),
+                                 BITWISE::_CMPLMNT(BITWISE::_bitmask(0 != x)));
+
+        for (std::size_t i = 1; i < N - 1; ++i) {
+            sum = BITWISE::_ADDMOD(sum,
+                                   BITWISE::_AND(
+                                       BITWISE::_constant(m_value[i]),
+                                       BITWISE::_CMPLMNT(BITWISE::_bitmask(i != x))));
+        }
+
+        return BITWISE::ADDMOD(sum,
+                               BITWISE::_AND(
+                                   BITWISE::_constant(m_value[N - 1]),
+                                   BITWISE::_CMPLMNT(BITWISE::_bitmask((N-1) != x))));
+    }
+
+private:
+    const std::vector<VAL> m_value;
+};
+
+template <typename FR> using
+array_uint8 = BitwiseLUT<AST_Node<Alg_uint8<FR>>,
+                         AST_Op<Alg_uint8<FR>>,
+                         std::uint8_t,
+                         BitwiseAST<Alg_uint8<FR>>>;
+
+template <typename FR> using
+array_uint32 = BitwiseLUT<AST_Node<Alg_uint32<FR>>,
+                          AST_Op<Alg_uint32<FR>>,
+                          std::uint32_t,
+                          BitwiseAST<Alg_uint32<FR>>>;
+
+template <typename FR> using
+array_uint64 = BitwiseLUT<AST_Node<Alg_uint64<FR>>,
+                          AST_Op<Alg_uint64<FR>>,
+                          std::uint64_t,
+                          BitwiseAST<Alg_uint64<FR>>>;
 
 } // namespace snarkfront
 
