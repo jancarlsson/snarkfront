34b5aee0075fb401c4e8e7a2fd04e60fb9eb4430
jancarlsson <jancarlsson@Safe-mail.net>
2015-06-24 01:24:49 +0000

    coding style edits

--- DSL_identity.cpp
+++ DSL_identity.cpp
@@ -9,39 +9,19 @@ namespace snarkfront {
 //
 
 // Boolean
-bool zero(const bool& dummy) {
-    return false;
-}
-
-bool one(const bool& dummy) {
-    return true;
-}
+bool zero(const bool& dummy) { return false; }
+bool one(const bool& dummy) { return true; }
 
 // 8-bit octet
-uint8_t zero(const uint8_t& dummy) {
-    return 0;
-}
-
-uint8_t one(const uint8_t& dummy) {
-    return 1;
-}
+uint8_t zero(const uint8_t& dummy) { return 0; }
+uint8_t one(const uint8_t& dummy) { return 1; }
 
 // 32-bit word
-uint32_t zero(const uint32_t& dummy) {
-    return 0;
-}
-
-uint32_t one(const uint32_t& dummy) {
-    return 1;
-}
+uint32_t zero(const uint32_t& dummy) { return 0; }
+uint32_t one(const uint32_t& dummy) { return 1; }
 
 // 64-bit word
-uint64_t zero(const uint64_t& dummy) {
-    return 0;
-}
-
-uint64_t one(const uint64_t& dummy) {
-    return 1;
-}
+uint64_t zero(const uint64_t& dummy) { return 0; }
+uint64_t one(const uint64_t& dummy) { return 1; }
 
 } // namespace snarkfront
