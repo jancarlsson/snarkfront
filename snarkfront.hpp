c860b84db5bfbc2796d28fdd8ab0af0a4c960df5
jancarlsson <jancarlsson@Safe-mail.net>
2015-06-24 01:42:33 +0000

    AES and SHA in cryptl

--- snarkfront.hpp
+++ snarkfront.hpp
@@ -26,7 +26,7 @@
 #include <snarkfront/GenericProgressBar.hpp>
 
 // input and printing of hexadecimal text
-#include <snarkfront/HexUtil.hpp>
+#include <snarkfront/HexDumper.hpp>
 
 // initialize elliptic curves
 #include <snarkfront/InitPairing.hpp>
@@ -35,22 +35,5 @@
 #include <snarkfront/MerkleAuthPath.hpp>
 #include <snarkfront/MerkleBundle.hpp>
 #include <snarkfront/MerkleTree.hpp>
-
-// Secure Hash Algorithms
-#include <snarkfront/SHA_1.hpp>
-#include <snarkfront/SHA_224.hpp>
-#include <snarkfront/SHA_256.hpp>
-#include <snarkfront/SHA_384.hpp>
-#include <snarkfront/SHA_512.hpp>
-#include <snarkfront/SHA_512_224.hpp>
-#include <snarkfront/SHA_512_256.hpp>
-
-// Advanced Encryption Algorithm
-#include <snarkfront/AdvancedEncryptionStd.hpp>
-#include <snarkfront/AES_Cipher.hpp>
-#include <snarkfront/AES_InvCipher.hpp>
-#include <snarkfront/AES_InvSBox.hpp>
-#include <snarkfront/AES_KeyExpansion.hpp>
-#include <snarkfront/AES_SBox.hpp>
 
 #endif
